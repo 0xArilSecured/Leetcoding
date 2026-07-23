7d949678c94df0688c2bc49ca73d6cb1c158b1f0
Aril Fadla Hudallah <arilhuda.career.2406@gmail.com>
2026-07-23 07:15:28 +0700

    Time: 42 ms (76.13%) | Memory: 19.2 MB (87.98%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,23 +1,15 @@
-'''
-Tentukan variabel dan nilai result berupa array kosong
-Iterate using while for both word
-While iterate do append for both word
-Cek salah satu dari sisa append dan tambahkan
-Join result to make a string as expected
-'''
-
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        w1, w2 = word1, word2
         i, j = 0, 0
-
         res = []
 
-        for i in range (min(len(w1), len(w2))):
-            res.append(w1[i])
-            res.append(w2[i])
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[i])
             i += 1
-        res.append(w1[i:])
-        res.append(w2[i:])
+            j += 1
+        
+        res.append(word1[i:])
+        res.append(word2[j:])
 
         return "".join(res)
